56aace5d2a05110850ab5258d36050910e8741a1
ja <jakub.atroszko@.gmail.com>
2016-12-20 20:01:26 +0100

    Interface user ready

--- visualstudio/bioinformatyka/bioinformatyka/Help.xaml.cs
+++ visualstudio/bioinformatyka/bioinformatyka/Help.xaml.cs
@@ -10,32 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace bioinformatyka
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Help.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Help : Window
     {
-        public MainWindow()
+        public Help()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            Blast b = new Blast();
-            b.Show();
-            //this.Close();
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            Help h = new Help();
-            h.Show();
         }
     }
 }
---
+++ visualstudio/bioinformatyka/bioinformatyka/Result.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace bioinformatyka
+{
+    /// <summary>
+    /// Interaction logic for Result.xaml
+    /// </summary>
+    public partial class Result : Window
+    {
+        public Result()
+        {
+            /*
+            InitializeComponent();
+            bool result = true;
+            var item;
+            if (result)
+            {
+                var item = new Image();
+            }
+            else
+            {
+                item = new TextBlock();
+            }
+
+            myCanvas.Children.Add(tb);
+            tb.Width = 100;
+            Canvas.SetLeft(tb, 50);
+            Canvas.SetTop(tb, 20);
+             * */
+        }
+    }
+}
